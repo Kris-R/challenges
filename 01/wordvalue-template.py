3dbf6b13e105f5db34e1eb849307f8b73c23e166
admin <KrisR@pythonanywhere.com>
2018-03-16 11:35:31 +0100

    worked on the wordvalue-template.py file to start calculating word values

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -2,15 +2,20 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    dictAsList = list(open(DICTIONARY))
-    return dictAsList[10:20]
+    dictAsList = [i.upper().rstrip() for i in open(DICTIONARY)]
+    return dictAsList
 
 print(load_words())
 
-def calc_word_value():
+def calc_word_value(load):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    wordValues = {}
+    for word in load:
+        for letter in word.split():
+            print(letter)
+
+print(calc_word_value(load_words()))
 
 def max_word_value():
     """Calculate the word with the max value, can receive a list
