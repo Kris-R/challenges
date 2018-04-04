3ed908c1037adf0cca0bea4125f4469dc077c7f2
admin <KrisR@pythonanywhere.com>
2018-04-04 19:48:05 +0200

    Changes

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -20,10 +20,21 @@ def calc_word_value(load):
 
 print(calc_word_value(load_words()))
 
-def max_word_value():
+def max_word_value(input=DICTIONARY):
     """Calculate the word with the max value, can receive a list
-    of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    of words as arg, if none provided uses default DICTIONARY
+
+    Notes 4.4.2018: so it *can* accept any list, but I have set the default as
+    the dictionary from data.py
+    Will test whether it's there or not, then run the calculations on word value
+    See test_wordvalue.py"""
+
+    if input == type(list)
+
+    else:
+        # use the dictionary in a calculating function/
+
+
 
 if __name__ == "__main__":
     pass # run unittests to validate
