0c9b2911686893daf02d07a0ac82173ab5dc7d7f
admin <KrisR@pythonanywhere.com>
2018-05-07 17:48:08 +0200

    OK. Made some headway in using product() and SequenceMatcher(), but for now when comparing ratios, the only ones returned (at condition > SIMILAR) are identical tags from each product() tag iteration. So not much help

--- 03/tags-help.py
+++ 03/tags-help.py
@@ -41,9 +41,13 @@ def get_similarities(tags):
     Hint 1: compare each tag, use for in for, or product from itertools (already imported)
     Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
     Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
-    help(SequenceMatcher)
-    s = SequenceMatcher(lambda x: x == " ", "Hello you;", "Smello you;")
-    print(s.ratio())
+    for pair in product(tags[0], tags[0]):
+        print(pair)
+        s = SequenceMatcher(lambda x: x == " ", pair[0], pair[1])
+            # Creates an object s from the difflib.SequenceMatcher class constructor
+        # if s.ratio() > SIMILAR:
+        #     print(pair)
+
 
 if __name__ == "__main__":
     tags = get_tags()
